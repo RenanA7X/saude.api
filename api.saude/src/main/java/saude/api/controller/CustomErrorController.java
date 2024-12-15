860f1b0721f21fcd22add233b67bff0c7b042230
abimaelnaftali <abimaelnaftali@gmail.com>
2024-12-15 13:45:37 -0300

    Página de erro

---
+++ api.saude/src/main/java/saude/api/controller/CustomErrorController.java
@@ -0,0 +1,32 @@
+package saude.api.controller;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private final ErrorAttributes errorAttributes;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @RequestMapping("/error")
+    public String handleError(WebRequest webRequest, Model model) {
+        Map<String, Object> errorDetails = errorAttributes.getErrorAttributes(
+            webRequest, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE, ErrorAttributeOptions.Include.STACK_TRACE)
+        );
+        model.addAttribute("status", errorDetails.get("status"));
+        model.addAttribute("error", errorDetails.get("error"));
+        model.addAttribute("message", errorDetails.get("message"));
+        return "error";
+    }
+}
